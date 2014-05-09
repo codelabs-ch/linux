e28891f582491c4464e9b2e89c34b4bf74abcb7f
Reto Buerki <reet@codelabs.ch>
2014-05-09 17:37:59 +0200

    Define channel_info struct in sinfo API
    Use this struct as argument in the muen_get_channel_info function.

--- include/muen/sinfo.h
+++ include/muen/sinfo.h
@@ -1,6 +1,8 @@
 #ifndef MUEN_SINFO_H
 #define MUEN_SINFO_H
 
+#define MAX_CHANNEL_NAME_LEN 63
+
 /*
  * Muen subject information API.
  *
@@ -8,16 +10,26 @@
  * of a Linux subject running on the Muen Separation Kernel.
  */
 
+/* Structure holding information about a Muen channel */
+struct muen_channel_info {
+	char name[MAX_CHANNEL_NAME_LEN + 1];
+	uint64_t address;
+	uint64_t size;
+	uint8_t event_number;
+	uint8_t vector;
+	bool writable;
+	bool has_event;
+	bool has_vector;
+};
+
 /*
  * Return information for a channel given by name.
  *
  * If no channel with given name exists, False is returned. The event_number
  * and vector parameters are only valid if indicated by the has_[event|vector]
- * parameters.
+ * struct members.
  */
 bool muen_get_channel_info(const char * const name,
-		uint64_t *address, uint64_t *size, bool *writable,
-		bool *has_event, uint8_t *event_number,
-		bool *has_vector, uint8_t *vector);
+		struct muen_channel_info *channel);
 
 #endif /* MUEN_SINFO_H */
