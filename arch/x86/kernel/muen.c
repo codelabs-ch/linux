2b43e1f95fb4b3084ac279d33f8fd843c374b86e
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2015-01-21 18:54:55 +0100

    muen: Set TSC-related CPU features
    Skip TSC clocksource checks during boot by setting the necessary
    capability bits. This change makes the 'tsc=reliable' boot parameter
    unnecessary.

--- arch/x86/kernel/muen.c
+++ arch/x86/kernel/muen.c
@@ -29,6 +29,12 @@ static unsigned long muen_sinfo_get_tsc_khz(void)
 	return muen_get_tsc_khz();
 }
 
+static void muen_set_cpu_features(struct cpuinfo_x86 *c)
+{
+	set_cpu_cap(c, X86_FEATURE_CONSTANT_TSC);
+	set_cpu_cap(c, X86_FEATURE_TSC_RELIABLE);
+}
+
 static void __init muen_platform_setup(void)
 {
 	x86_platform.calibrate_tsc = muen_sinfo_get_tsc_khz;
@@ -40,8 +46,9 @@ static bool __init muen_platform(void)
 }
 
 const __refconst struct hypervisor_x86 x86_hyper_muen = {
-	.name		= "Muen SK",
-	.detect		= muen_platform,
-	.init_platform	= muen_platform_setup,
+	.name			= "Muen SK",
+	.detect			= muen_platform,
+	.set_cpu_features	= muen_set_cpu_features,
+	.init_platform		= muen_platform_setup,
 };
 EXPORT_SYMBOL(x86_hyper_muen);
