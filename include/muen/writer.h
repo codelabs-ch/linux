404d393c347de212759d3f039fceeef9edf16166
Reto Buerki <reet@codelabs.ch>
2015-05-07 12:01:02 +0200

    muchannel: Rename writer functions
    Add muen_channel prefix to the muchannel writer functions.

--- include/muen/writer.h
+++ include/muen/writer.h
@@ -6,17 +6,18 @@
 /**
  * Initialize channel with given parameters.
  */
-void muchannel_initialize(struct muchannel *channel, const u64 protocol,
-			  const u64 size, const u64 elements, const u64 epoch);
+void muen_channel_init_writer(struct muchannel *channel, const u64 protocol,
+			      const u64 size, const u64 elements,
+			      const u64 epoch);
 
 /**
  * Deactivate channel.
  */
-void muchannel_deactivate(struct muchannel *channel);
+void muen_channel_deactivate(struct muchannel *channel);
 
 /**
  * Write element to given channel.
  */
-void muchannel_write(struct muchannel *channel, const void *const element);
+void muen_channel_write(struct muchannel *channel, const void *const element);
 
 #endif /* MUEN_CHANNEL_WRITER_H */
