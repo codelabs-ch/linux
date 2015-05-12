4640ad8dc46634475fca6dbde68c886bb028a132
Reto Buerki <reet@codelabs.ch>
2015-05-12 10:14:15 +0200

    muchannel: Replace elements argument with channel_size
    Rename the elements argument of the muen_channel_init_writer function to
    channel_size and subtract the channel header from it before calculating
    the elements count.

--- drivers/muen/channel/writer.c
+++ drivers/muen/channel/writer.c
@@ -16,17 +16,20 @@
 #include <muen/writer.h>
 
 void muen_channel_init_writer(struct muchannel *channel, const u64 protocol,
-			      const u64 element_size, const u64 elements,
+			      const u64 element_size, const u64 channel_size,
 			      const u64 epoch)
 {
-	muen_channel_deactivate(channel);
+	u64 data_size;
 
+	muen_channel_deactivate(channel);
 	memset(channel, 0, sizeof(struct muchannel));
+
+	data_size = channel_size - sizeof(struct muchannel_header);
 
 	atomic64_set(&channel->hdr.transport, SHMSTREAM20);
 	atomic64_set(&channel->hdr.protocol, protocol);
 	atomic64_set(&channel->hdr.size, element_size);
-	atomic64_set(&channel->hdr.elements, elements);
+	atomic64_set(&channel->hdr.elements, data_size / element_size);
 	atomic64_set(&channel->hdr.wsc, 0);
 	atomic64_set(&channel->hdr.wc, 0);
 
