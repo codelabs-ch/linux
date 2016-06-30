416a43792087c8bb241dd946bd1a577961532b7a
Reto Buerki <reet@codelabs.ch>
2016-06-30 10:09:39 +0200

    muen: Do not mark TSC as reliable
    Also remove TSC calibration override. This is in preparation of using a
    real Muen clocksource instead of the tsc clocksource with a pvirt
    read_tsc function. The latter can no longer be customized via pvops in
    newer kernels.

--- arch/x86/kernel/muen.c
+++ arch/x86/kernel/muen.c
@@ -1,5 +1,5 @@
 /*
- * Muen Hypervisor Detection code
+ * Muen platform setup code
  *
  * Copyright (C) 2014  Reto Buerki <reet@codelabs.ch>
  * Copyright (C) 2014  Adrian-Ken Rueegsegger <ken@codelabs.ch>
@@ -25,16 +25,6 @@
 #include <muen/sinfo.h>
 #include <muen/pci.h>
 
-static unsigned long muen_sinfo_get_tsc_khz(void)
-{
-	return muen_get_tsc_khz();
-}
-
-static void muen_set_cpu_features(struct cpuinfo_x86 *c)
-{
-	set_cpu_cap(c, X86_FEATURE_CONSTANT_TSC);
-	set_cpu_cap(c, X86_FEATURE_TSC_RELIABLE);
-}
 
 static void __init muen_init_IRQ(void)
 {
@@ -50,10 +40,9 @@ static void __init muen_init_IRQ(void)
 
 static void __init muen_platform_setup(void)
 {
-	x86_platform.calibrate_tsc = muen_sinfo_get_tsc_khz;
-	x86_init.irqs.intr_init    = muen_init_IRQ;
+	x86_init.irqs.intr_init	= muen_init_IRQ;
 #ifdef CONFIG_MUEN_PCI_MSI
-	x86_init.pci.arch_init     = muen_msi_init;
+	x86_init.pci.arch_init	= muen_msi_init;
 #endif
 
 	null_legacy_pic.nr_legacy_irqs = NR_IRQS_LEGACY;
@@ -67,9 +56,8 @@ static uint32_t __init muen_platform(void)
 }
 
 const __refconst struct hypervisor_x86 x86_hyper_muen = {
-	.name			= "Muen SK",
-	.detect			= muen_platform,
-	.set_cpu_features	= muen_set_cpu_features,
-	.init_platform		= muen_platform_setup,
+	.name		= "Muen SK",
+	.detect		= muen_platform,
+	.init_platform	= muen_platform_setup,
 };
 EXPORT_SYMBOL(x86_hyper_muen);
