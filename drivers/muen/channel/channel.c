db82c1d81800b4bebff7d344b8b7a24f655b099e
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2017-03-04 11:01:23 +0100

    Add support for Muen shared memory channels
    Muen shared memory channels are an implementation of the SHMStream
    Version 2 IPC protocol (shmstream).

---
+++ drivers/muen/channel/channel.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2013, 2015  Reto Buerki <reet@codelabs.ch>
+ * Copyright (C) 2013, 2015  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/export.h>
+
+#include <muen/channel.h>
+
+bool muen_channel_is_active(const struct muchannel * const channel)
+{
+	return atomic64_read(&channel->hdr.epoch) != MUCHANNEL_NULL_EPOCH;
+}
+EXPORT_SYMBOL(muen_channel_is_active);
---
+++ include/muen/channel.h
@@ -0,0 +1,37 @@
+#ifndef MUEN_CHANNEL_H
+#define MUEN_CHANNEL_H
+
+/*
+ * Muen shared memory channels.
+ *
+ * Muen shared memory channels are an implementation of the SHMStream Version 2
+ * IPC protocol (shmstream) as specified by 'SHMStream Version 2 IPC Interface'.
+ */
+
+#define SHMSTREAM20		0x487312b6b79a9b6dULL
+#define MUCHANNEL_NULL_EPOCH	0
+
+#include <linux/atomic.h>
+
+struct muchannel_header {
+	atomic64_t transport;
+	atomic64_t epoch;
+	atomic64_t protocol;
+	atomic64_t size;
+	atomic64_t elements;
+	u64 __reserved;
+	atomic64_t wsc;
+	atomic64_t wc;
+} __packed __aligned(8);
+
+struct muchannel {
+	struct muchannel_header hdr;
+	char data[];
+};
+
+/*
+ * Returns True if the channel is currently active.
+ */
+bool muen_channel_is_active(const struct muchannel * const channel);
+
+#endif /* MUEN_CHANNEL_H */
