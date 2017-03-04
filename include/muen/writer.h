f4931c2bb8ccebabd932343ed9c60a40d4b53854
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2017-03-04 11:01:23 +0100

    Add support for Muen shared memory channels
    Muen shared memory channels are an implementation of the SHMStream
    Version 2 IPC protocol (shmstream).

---
+++ include/muen/writer.h
@@ -0,0 +1,23 @@
+#ifndef MUEN_WRITER_H
+#define MUEN_WRITER_H
+
+#include <muen/channel.h>
+
+/**
+ * Initialize channel with given parameters.
+ */
+void muen_channel_init_writer(struct muchannel *channel, const u64 protocol,
+			      const u64 element_size, const u64 channel_size,
+			      const u64 epoch);
+
+/**
+ * Deactivate channel.
+ */
+void muen_channel_deactivate(struct muchannel *channel);
+
+/**
+ * Write element to given channel.
+ */
+void muen_channel_write(struct muchannel *channel, const void * const element);
+
+#endif /* MUEN_WRITER_H */
