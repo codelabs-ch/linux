e2d0699e470bb50e52fe65ce88f7dc66fa08263b
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2017-03-04 11:01:23 +0100

    Add support for Muen shared memory channels
    Muen shared memory channels are an implementation of the SHMStream
    Version 2 IPC protocol (shmstream).

---
+++ include/muen/reader.h
@@ -0,0 +1,42 @@
+#ifndef MUEN_READER_H
+#define MUEN_READER_H
+
+#include <muen/channel.h>
+
+enum muchannel_reader_result {
+	MUCHANNEL_INACTIVE,
+	MUCHANNEL_INCOMPATIBLE_INTERFACE,
+	MUCHANNEL_EPOCH_CHANGED,
+	MUCHANNEL_NO_DATA,
+	MUCHANNEL_OVERRUN_DETECTED,
+	MUCHANNEL_SUCCESS
+};
+
+struct muchannel_reader {
+	u64 epoch;
+	u64 protocol;
+	u64 size;
+	u64 elements;
+	u64 rc;
+};
+
+/*
+ * Initialize reader with given protocol.
+ */
+void muen_channel_init_reader(struct muchannel_reader *reader, u64 protocol);
+
+/*
+ * Read next element from given channel.
+ */
+enum muchannel_reader_result muen_channel_read(
+		const struct muchannel * const channel,
+		struct muchannel_reader *reader,
+		void *element);
+
+/*
+ * Drain all current channel elements.
+ */
+void muen_channel_drain(const struct muchannel * const channel,
+			struct muchannel_reader *reader);
+
+#endif /* MUEN_READER_H */
