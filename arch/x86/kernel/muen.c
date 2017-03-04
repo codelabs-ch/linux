fd6b58906667ebb68cb66ed8f54791ab2329ebfd
Reto Buerki <reet@codelabs.ch>
2017-03-04 11:39:00 +0100

    Add Muen hypervisor detection code
    The detection mechanism checks if the sinfo page magic is present. If
    the Muen SK/hypervisor is detected, override IRQ and PCI init functions
    and clear the TSC CPU feature since accessing the Timestamp counter is
    not allowed on this platform.
    
    To avoid unnecessary traps due to TSC access get_cycles() is aliased to
    the muen_sched_clock_read() function.

---
+++ arch/x86/kernel/muen.c
@@ -0,0 +1,63 @@
+/*
+ * Muen platform setup code
+ *
+ * Copyright (C) 2014  Reto Buerki <reet@codelabs.ch>
+ * Copyright (C) 2014  Adrian-Ken Rueegsegger <ken@codelabs.ch>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/irq.h>
+#include <linux/io.h>
+
+#include <asm/i8259.h>
+#include <asm/x86_init.h>
+#include <asm/hypervisor.h>
+#include <asm/cpufeature.h>
+
+#include <muen/sinfo.h>
+#include <muen/pci.h>
+
+static int muen_pic_probe(void) { return NR_IRQS_LEGACY; }
+
+static void __init muen_init_IRQ(void)
+{
+	native_init_IRQ();
+	init_ISA_irqs();
+}
+
+static void __init muen_platform_setup(void)
+{
+	setup_clear_cpu_cap(X86_FEATURE_TSC);
+
+	x86_init.irqs.intr_init	= muen_init_IRQ;
+#ifdef CONFIG_MUEN_PCI_MSI
+	x86_init.pci.arch_init	= muen_msi_init;
+#endif
+
+	null_legacy_pic.nr_legacy_irqs = NR_IRQS_LEGACY;
+	null_legacy_pic.probe          = muen_pic_probe;
+	legacy_pic = &null_legacy_pic;
+}
+
+static uint32_t __init muen_platform(void)
+{
+	muen_sinfo_early_init();
+	return muen_check_magic();
+}
+
+const __initconst struct hypervisor_x86 x86_hyper_muen = {
+	.name		    = "Muen SK",
+	.detect		    = muen_platform,
+	.type		    = X86_HYPER_MUEN,
+	.init.init_platform = muen_platform_setup,
+};
+EXPORT_SYMBOL(x86_hyper_muen);
