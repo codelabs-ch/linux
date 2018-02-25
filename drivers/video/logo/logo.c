247814faff431f99bfc368c4ba8b7a57f9487e38
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2018-02-25 21:45:02 +0100

    video/logo: add Muen/Linux logo for framebuffer console

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -111,6 +111,11 @@ const struct linux_logo * __init_refok fb_find_logo(int depth)
 		/* M32R Linux logo */
 		logo = &logo_m32r_clut224;
 #endif
+#ifdef CONFIG_LOGO_MUEN_CLUT224
+		/* Muen Linux logo */
+		logo = &logo_muen_clut224;
+#endif
+
 	}
 	return logo;
 }
