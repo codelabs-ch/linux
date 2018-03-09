ee3208e4f993c9efcdb97897ca681245ec05e0be
Reto Buerki <reet@codelabs.ch>
2018-03-09 10:09:59 +0100

    Implement IRQ affinity handling
    Let CPU cores store the device IRQs of associated devices in a global
    list accessible to all CPU cores. This is required because the setup of
    a device can happen on any core (missing the IRQ information in its
    sinfo as the IRQ might not be assigned to this core).
    
    Adjust muen-msi driver to use the new muen_smp_get_irq_affinity function
    to retrieve the CPU core which is responsible to handle IRQ(s) of a
    given device.

--- include/muen/smp.h
+++ include/muen/smp.h
@@ -6,4 +6,14 @@
  */
 void __init muen_smp_init(void);
 
+/*
+ * Return IRQ affinity information for PCI device with given SID.
+ *
+ * The function returns null if no IRQ affinity information for the specified
+ * device exists. If IRQ affinity information is found, the cpu argument
+ * specifies the CPU which is responsible for the IRQ(s) of the device.
+ */
+const struct muen_device_type *const
+muen_smp_get_irq_affinity(const uint16_t sid, unsigned int *cpu);
+
 #endif /* MUEN_SMP_H */
