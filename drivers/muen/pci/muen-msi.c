985fe64a87e1587179855a012449a73831eae16d
Reto Buerki <reet@codelabs.ch>
2018-07-26 17:31:17 +0200

    Make per-core IRQs work
    Rework CPU affinity database in order to query multiple resources per
    device. This is required to acquire CPU affinity information of IRQs
    which are distributed among CPU cores.
    
    Adjust muen-input, muen-msi and muen-smp drivers accordingly.

--- drivers/muen/pci/muen-msi.c
+++ drivers/muen/pci/muen-msi.c
@@ -81,9 +81,9 @@ static int muen_setup_msi_irq(struct pci_dev *dev, struct msi_desc *msidesc,
 }
 
 static int muen_irq_alloc_descs(struct pci_dev *dev, const unsigned int virq,
-				const unsigned int cnt, const unsigned int cpu)
+				const unsigned int cnt)
 {
-	int i, alloc;
+	int alloc;
 
 	alloc = irq_alloc_descs(dev->irq, dev->irq, cnt,
 				dev_to_node(&dev->dev));
@@ -98,32 +98,72 @@ static int muen_irq_alloc_descs(struct pci_dev *dev, const unsigned int virq,
 		irq_free_descs(dev->irq, cnt);
 		return -EINVAL;
 	}
-
-	for (i = 0; i < cnt; i++)
-		per_cpu(vector_irq, cpu)[virq + i] = irq_to_desc(dev->irq + i);
-
 	return 0;
 }
 
-static void muen_irq_free_descs(const unsigned int irq, const unsigned int virq,
-				const unsigned int cnt)
+/*
+ * Free IRQ descriptor and unset per-core vector.
+ * Assumes the caller to be the CPU owning the IRQ.
+ */
+static void muen_irq_free_desc(const unsigned int irq, const unsigned int virq)
 {
-	int i;
+	irq_free_descs(irq, 1);
+	__this_cpu_write(vector_irq[virq], VECTOR_UNUSED);
+}
 
-	irq_free_descs(irq, cnt);
-	for (i = 0; i < cnt; i++)
-		__this_cpu_write(vector_irq[virq + i], VECTOR_UNUSED);
+static bool
+muen_match_devsid(const struct muen_cpu_affinity *const affinity, void *data)
+{
+	const uint16_t *const sid = data;
+
+	return affinity->res.kind == MUEN_RES_DEVICE
+		&& affinity->res.data.dev.sid == *sid;
+}
+
+/*
+ * Return overall IRQ count of device affinity entries in given list and set
+ * data pointer to device resource with smallest IRQ value.
+ */
+unsigned int muen_devres_data(const struct muen_cpu_affinity *const affinity,
+		const struct muen_device_type **data)
+{
+	struct muen_cpu_affinity *entry;
+	unsigned int irq_start = 255 + 1, irq_count = 0;
+
+	list_for_each_entry(entry, &affinity->list, list) {
+		irq_count += entry->res.data.dev.ir_count;
+		if (entry->res.data.dev.irq_start < irq_start) {
+			irq_start = entry->res.data.dev.irq_start;
+			*data = &entry->res.data.dev;
+		}
+	}
+	return irq_count;
+}
+
+static void
+muen_setup_cpu_vector_irqs(const struct muen_cpu_affinity *const affinity)
+{
+	unsigned int i;
+	struct muen_cpu_affinity *entry;
+	struct muen_device_type *dev;
+
+	list_for_each_entry(entry, &affinity->list, list) {
+		dev = &entry->res.data.dev;
+		for (i = 0; i < dev->ir_count; i++)
+			per_cpu(vector_irq, entry->cpu)[dev->irq_start + i] =
+				irq_to_desc(dev->irq_start
+						- ISA_IRQ_VECTOR(0) + i);
+	}
 }
 
 static int muen_setup_msi_irqs(struct pci_dev *dev, int nvec, int type)
 {
 	struct msi_desc *msidesc;
 	int ret;
-	unsigned int irq, virq, cpu;
+	unsigned int irq, irq_count, virq, affinity_count;
+	struct muen_cpu_affinity affinity;
 	const uint16_t sid = PCI_DEVID(dev->bus->number, dev->devfn);
-
-	const struct muen_device_type *const
-		dev_info = muen_smp_get_irq_affinity(sid, &cpu);
+	const struct muen_device_type *dev_data;
 
 	/* Multiple vectors only supported for MSI-X */
 	if (nvec > 1 && type == PCI_CAP_ID_MSI) {
@@ -131,44 +171,55 @@ static int muen_setup_msi_irqs(struct pci_dev *dev, int nvec, int type)
 		return 1;
 	}
 
-	if (!dev_info) {
+	affinity_count = muen_smp_get_res_affinity(&affinity,
+			&muen_match_devsid, (void *)&sid);
+	if (list_empty(&affinity.list)) {
 		dev_err(&dev->dev, "Error retrieving Muen device info for SID 0x%x\n",
 			sid);
 		return -EINVAL;
 	}
-	if (nvec > dev_info->ir_count) {
+
+	irq_count = muen_devres_data(&affinity, &dev_data);
+	if (nvec > irq_count) {
 		dev_err(&dev->dev, "Device requests more IRQs than allowed by policy (%d > %d)\n",
-			nvec, dev_info->ir_count);
-		return -EINVAL;
+			nvec, irq_count);
+		ret = -EINVAL;
+		goto error_free_affinity;
 	}
-	if (!(dev_info->flags & DEV_MSI_FLAG)) {
+	if (!(dev_data->flags & DEV_MSI_FLAG)) {
 		dev_err(&dev->dev, "Device not configured for MSI\n");
-		return -EINVAL;
+		ret = -EINVAL;
+		goto error_free_affinity;
 	}
 
-	virq = dev_info->irq_start;
+	virq = dev_data->irq_start;
 	dev->irq = virq - ISA_IRQ_VECTOR(0);
 	if (dev->irq >= NR_IRQS_LEGACY) {
-		ret = muen_irq_alloc_descs(dev, virq, nvec, cpu);
+		ret = muen_irq_alloc_descs(dev, virq, nvec);
 		if (ret < 0)
-			return ret;
+			goto error_free_affinity;
 	}
 
 	irq = dev->irq;
 	list_for_each_entry(msidesc, dev_to_msi_list(&dev->dev), list) {
 		ret = muen_setup_msi_irq(dev, msidesc, irq,
-					 dev_info->irte_start);
+					 dev_data->irte_start);
 		if (ret < 0)
-			goto error;
+			goto error_free_descs;
 
 		dev_info(&dev->dev, "IRQ %d for MSI%s\n", irq,
 			 type == PCI_CAP_ID_MSIX ? "-X" : "");
 		irq++;
 	}
+
+	muen_setup_cpu_vector_irqs(&affinity);
+	muen_smp_free_res_affinity(&affinity);
 	return 0;
 
-error:
-	muen_irq_free_descs(dev->irq, virq, nvec);
+error_free_descs:
+	irq_free_descs(irq, nvec);
+error_free_affinity:
+	muen_smp_free_res_affinity(&affinity);
 	return ret;
 }
 
@@ -176,7 +227,7 @@ static void muen_teardown_msi_irq(unsigned int irq)
 {
 	/* Do not destroy legacy IRQs */
 	if (irq >= NR_IRQS_LEGACY && irq_to_desc(irq))
-		muen_irq_free_descs(irq, irq + ISA_IRQ_VECTOR(0), 1);
+		muen_irq_free_desc(irq, irq + ISA_IRQ_VECTOR(0));
 }
 
 int __init muen_msi_init(void)
