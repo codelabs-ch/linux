853c26d841bcddea8540d7bc12a3ed39f060a124
Reto Buerki <reet@codelabs.ch>
2020-05-13 18:18:05 +0200

    muen-smp: Introduce muen_smp_one_match function
    The function fills in resource data for the resource identified by name
    and resource kind. Only one match is expected and the caller must
    provide the memory for the result.
    
    Returns true if exactly one resource match is found, false otherwise. If
    true, the result contains the requested resource information.

--- include/muen/smp.h
+++ include/muen/smp.h
@@ -21,13 +21,24 @@ typedef bool (*match_func)(const struct muen_cpu_affinity *const aff,
 
 /*
  * Get list of CPU affinity entries for which the given match function
- * evaluates to true.
+ * evaluates to true. The caller must free the result.
  *
  * Returns the affinity entry count on success, a negative value on error. If
  * the match function is NULL, all available affinity records are returned.
  */
 int muen_smp_get_res_affinity(struct muen_cpu_affinity *const result,
 		match_func func, void *match_data);
+
+/*
+ * Fill in resource data for resource identified by given name and resource
+ * kind. Only one match is expected and the caller must allocate the memory for
+ * the result.
+ *
+ * Returns true if exactly one resource match is found, false otherwise. If
+ * true, result contains the requested resource information.
+ */
+bool muen_smp_one_match(struct muen_cpu_affinity *const result,
+		const char *const name, const enum muen_resource_kind kind);
 
 /*
  * Remove and free elements of given CPU affinity list.
