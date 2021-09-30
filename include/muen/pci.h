5cf9989b5cd9fb8a22d3d4e73405bf9e1f9a9b8c
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2021-09-30 22:47:05 +0200

    muen/pci: Wrap Muen MSI management into irqdomain
    Make Muen MSI handling irq domain compatible, analogous to commit
    2e4386e.

--- include/muen/pci.h
+++ include/muen/pci.h
@@ -1,9 +1,16 @@
 #ifndef MUEN_PCI_H
 #define MUEN_PCI_H
 
+#include <linux/irqdomain.h>
+
 /**
  * Initialize Muen PCI driver.
  */
 int __init muen_pci_init(void);
+
+/**
+ * Create Muen PCI-MSI IRQ Domain.
+ */
+struct irq_domain * __init muen_create_pci_msi_domain(void);
 
 #endif /* MUEN_PCI_H */
