805f6cbe2abd7b1a5a03666667503fdb1d15a771
David Loosli <david@codelabs.ch>
2022-12-16 14:53:01 +0100

    Muen SK - GIC Fix / Refactoring
    refactor Muen SK irq chip driver for
    Linux subjects to match the latest
    Muen SK interrupt virtualization
    approch
    
    issue: MOA-linux-02

--- drivers/irqchip/irq-muensk.c
+++ drivers/irqchip/irq-muensk.c
@@ -18,13 +18,24 @@
 *                                                                        *
 *                                                                        *
 *    @contributors                                                       *
-*        2019, 2021  David Loosli <dave@codelabs.ch>                     *
+*        2019, 2022  David Loosli <dave@codelabs.ch>                     *
 *                                                                        *
 *                                                                        *
 *    @description                                                        *
-*        irq-muensk Linux subject Muen SK interrupt chip implementation  *
-*        as the counterpart of the MuenSK GIC implementation (needed for *
-*        all Linux subjects running on the Muen SK)                      *
+*        irq-muensk contains the Muen SK irq chip implementation that    *
+*        is required by all Linux subjects of a Muen SK system. This     *
+*        driver acts as counterpart to the GIC virtualization provided   *
+*        by the Muen SK. The driver currently only supports a static     *
+*        configuration of the virtual CPU interface with only group 0    *
+*        interrupts, separate priority drop and deactivation (i.e. EOI   *
+*        mode equ. 1) and default priority and binary point values. As   *
+*        the official ARM GICv2 implementation and its derivatives, the  *
+*        Muen SK acknowledges and drops the priority in the exception    *
+*        irq entry function to be able to let the Linux kernel handle    *
+*        all interrupts as edge-triggerd, even though most hardware      *
+*        interrupts are defined as level-sensitive by the actual ARM     *
+*        and SoC specification. This also allows the driver to let the   *
+*        ack, mask and unmask function implementations empty.            *
 *    @project                                                            *
 *        MuenOnARM                                                       *
 *    @interface                                                          *
@@ -43,15 +54,15 @@
 #include "irq-muensk.h"
 
 /*
- * definitions
+ * Definitions
  */
 static const struct irq_chip muensk_chip = {
-	.name					= "Muen SK - (virtual) IRQ Chip, version 0",
-	.irq_mask				= muensk_mask,
-	.irq_unmask				= muensk_unmask,
-	.irq_ack				= muensk_ack,
-	.irq_eoi				= muensk_eoi,
-	.flags					= IRQCHIP_SKIP_SET_WAKE,
+	.name           = "Muen SK - (virtual) IRQ Chip, version 0.9",
+	.irq_mask       = muensk_mask,
+	.irq_unmask     = muensk_unmask,
+	.irq_ack        = muensk_ack,
+	.irq_eoi        = muensk_eoi,
+	.flags          = IRQCHIP_SKIP_SET_WAKE,
 };
 
 static const struct irq_domain_ops muensk_irq_domain_ops = {
@@ -62,119 +73,196 @@ static const struct irq_domain_ops muensk_irq_domain_ops = {
 
 struct muensk_irq_data {
 	struct irq_chip chip;
-	unsigned long distributor_physical_address;
-	void __iomem * distributor_raw_address;
-	unsigned long cpu_interface_physical_address;
-	void __iomem * cpu_interface_raw_address;
+	unsigned long physical_address;
+	void __iomem * raw_address;
 	struct irq_domain * domain;
 	bool initialized;
 };
 
 static struct muensk_irq_data muensk_data;
 
-/*
- * ToDo
+/**
+ * muensk_irq_domain_map - Maps an interrupt based on its hardware
+ * id and its type, i.e. software generated and private peripheral
+ * interrupts per cpu with the respective domain info, flags and
+ * irq handler function and shared peripheral interrupts with no
+ * autoprobing and the fast EOI handler (c.f. description). This
+ * function is part of the irq domain ops specification.
+ *
+ * @d  :	the interrupt domain
+ * @irq:	the software interrupt number
+ * @hw :	the hardware interrupt number
+ *
+ * Returns 0 for success or an error (c.f. kernel/irq/irqdesc.c,
+ * irq_set_percpu_devid_partition) otherwise.
  */
-static int muensk_irq_domain_map(struct irq_domain *d, unsigned int irq, irq_hw_number_t hw)
+static int muensk_irq_domain_map(
+	struct irq_domain *d, unsigned int irq, irq_hw_number_t hw
+)
 {
-	printk(KERN_INFO "MuenSK IRQ Chip - domain map the IRQ No: %ld", hw);
-
-	if (is_sgi_interrupt(hw) || is_ppi_interrupt(hw)) {
-		irq_set_percpu_devid(irq);
+	printk(KERN_DEBUG "Muen SK IRQ Chip - domain map the IRQ No: %ld", hw);
+	if (is_sgi_interrupt(hw) || is_ppi_interrupt(hw))
+	{
 		irq_domain_set_info(d, irq, hw, &(muensk_data.chip), d->host_data,
 			handle_percpu_devid_irq, NULL, NULL);
-		irq_set_status_flags(irq, IRQ_NOAUTOEN);
-	} else {
+		return irq_set_percpu_devid(irq);
+	}
+	else
+	{
 		irq_domain_set_info(d, irq, hw, &(muensk_data.chip), d->host_data,
 			handle_fasteoi_irq, NULL, NULL);
-		irq_clear_status_flags(irq, IRQ_NOPROBE);
+		irq_set_noprobe(irq);
+		return 0;
 	}
-	return 0;
 }
 
+/**
+ * muensk_irq_domain_unmap - Unmaps an interrupt based on the
+ * irq domain reset function. This function is part of the irq
+ * domain ops specification.
+ *
+ * @d  :	the interrupt domain
+ * @irq:	the software interrupt number
+ */
 static void muensk_irq_domain_unmap(struct irq_domain *d, unsigned int irq)
 {
-	/*
-	 * currently not implmeneted (no ARMv8 GICv2 distributor
-	 * virtualization, Muen SK core does this and has full
-	 * control over the interrupt assignment to subjects)
-	 */
-    printk(KERN_INFO "MuenSK IRQ Chip - domain unmap the IRQ No: %d", irq);
+	printk(KERN_DEBUG "Muen SK IRQ Chip - domain unmap the IRQ No: %d", irq);
+	irq_domain_reset_irq_data(irq_get_irq_data(irq));
 }
 
-static int muensk_irq_domain_xlate(struct irq_domain *d, struct device_node *ctrlr,
+/**
+ * muensk_irq_domain_xlate - Translates the interrupt properties
+ * provided by the device tree according to the interrupt type.
+ * This function is part of the irq domain ops specification.
+ *
+ * @d         :	the interrupt domain
+ * @ctrlr     :	a device tree node
+ * @intspec   :	the interrupt specification of the node
+ * @intsize   :	the array size of the interrupt specification
+ * @out_hwirq :	the hardware interrupt number (out parameter)
+ * @out_type  :	the hardware interrupt type (out parameter)
+ *
+ * Returns 0 for success or an EINVAL error, if the number
+ * of interrupt entries is not equal to 3 (i.e. software
+ * generated vs. private peripheral vs. shared peripheral
+ * for [0], hardware irq id for [1] and interrupt type for
+ * [2] with edge, level etc.).
+ */
+static int muensk_irq_domain_xlate(
+	struct irq_domain *d, struct device_node *ctrlr,
 	const u32 *intspec, unsigned int intsize,
-	unsigned long *out_hwirq, unsigned int *out_type)
+	unsigned long *out_hwirq, unsigned int *out_type
+)
 {
-	if (intspec[0] == SGI_INTERRUPT_TYPE) {
+	if (WARN_ON(intsize != 3))
+	{
+		return -EINVAL;
+	}
+
+	printk(KERN_DEBUG "Muen SK IRQ Chip - domain xlate with IRQ specification: %d / %d / %d",
+	          intspec[0], intspec[1], intspec[2]);
+
+	if (intspec[0] == SGI_INTERRUPT_TYPE)
+	{
 		*out_hwirq = intspec[1];
 		*out_type  = intspec[2] & IRQ_TYPE_SENSE_MASK;
 	}
 
-	if (intspec[0] == PPI_INTERRUPT_TYPE) {
+	if (intspec[0] == PPI_INTERRUPT_TYPE)
+	{
 		*out_hwirq = intspec[1] + 16;
 		*out_type  = intspec[2] & IRQ_TYPE_SENSE_MASK;
 	}
 
-	if (intspec[0] == SPI_INTERRUPT_TYPE) {
+	if (intspec[0] == SPI_INTERRUPT_TYPE)
+	{
 		*out_hwirq = intspec[1] + 32;
 		*out_type  = intspec[2] & IRQ_TYPE_SENSE_MASK;
 	}
 
-	/*
-	 * Device Tree Specification Error Check
-	 */
 	WARN_ON(*out_type == IRQ_TYPE_NONE);
-
 	return 0;
 }
 
-/*
- * ToDo
+/**
+ * muensk_mask - Masking should disable the signaling of an
+ * interrupt to the core, but is neither required for this
+ * approach (c.f. description) nor (yet) supported. For a
+ * Muen SK system unmasking has to be provided by the
+ * hypervisor and therefore would have to make use of an
+ * HVC call and parameter passing via registers. This
+ * function is part of the irq chip ops specification.
+ *
+ * @data :	the interrupt data
  */
 void muensk_mask(struct irq_data *data)
 {
-	/*
-	 * currently not implmeneted (no ARMv8 GICv2 distributor
-	 * virtualization, Muen SK core does this and has full
-	 * control over the interrupt assignment to subjects)
-	 */
-    printk(KERN_INFO "MuenSK IRQ Chip - mask called with IRQ No: %ld", data->hwirq);
+	printk(KERN_DEBUG "Muen SK IRQ Chip - mask called with IRQ No: %ld", data->hwirq);
 }
 
+/**
+ * muensk_unmask - Unmasking should enable the signaling of
+ * an interrupt to the core, but is neither required for
+ * this approach (c.f. description) nor (yet) supported.
+ * For a Muen SK system unmasking has to be provided by
+ * the hypervisor and therefore would have to make use of
+ * an HVC call and parameter passing via registers. This
+ * function is part of the irq chip ops specification.
+ *
+ * @data :	the interrupt data
+ */
 void muensk_unmask(struct irq_data *data)
 {
-	/*
-	 * currently not implmeneted (no ARMv8 GICv2 distributor
-	 * virtualization, Muen SK core does this and has full
-	 * control over the interrupt assignment to subjects)
-	 */
-    printk(KERN_INFO "MuenSK IRQ Chip - unmask called with IRQ No: %ld", data->hwirq);
+	printk(KERN_DEBUG "Muen SK IRQ Chip - unmask called with IRQ No: %ld", data->hwirq);
 }
 
-void muensk_ack(struct irq_data *data)
-{
-	/* do nothing due to calling sequence linux vs. ARM GICv2 */
-}
+/**
+ * muensk_ack - Acknowledging should mark an interrupt to
+ * be actively handled, but is not required for this
+ * approach (c.f. description). This function is part of
+ * the irq chip ops specification.
+ *
+ * @data :	the interrupt data
+ */
+void muensk_ack(struct irq_data *data) { }
 
+/**
+ * muensk_ack - Signals the end of the interrupt handling.
+ * For the currently used approach, this is done by writing
+ * to the deactivation register (c.f. description). This
+ * function is part of the irq chip ops specification.
+ *
+ * @data :	the interrupt data
+ */
 void muensk_eoi(struct irq_data *data)
 {
-	writel_relaxed(data->hwirq, muensk_data.cpu_interface_raw_address + IRQ_ALIASED_END_OF_INTERRUPT_OFFSET);
+	writel_relaxed(data->hwirq, muensk_data.raw_address + IRQ_DEACTIVATE_INTERRUPT_OFFSET);
 	isb();
 }
 
-/*
- * ToDo
+/**
+ * muensk_handle_irq - Called by the Linux kernel scheduling
+ * and exception handling process for every interrupt raised
+ * on the core's interface. For the currently used approach,
+ * this function acknowledges and drops the priority of the
+ * interrupt to "simulate" an edge triggered Linux irq (c.f.
+ * description).
+ *
+ * @regs :	the registers stored at exception entry
  */
 static void __exception_irq_entry muensk_handle_irq(struct pt_regs *regs)
 {
 	u32 irq_status, irq_number;
 
-	do {
-		irq_status = readl_relaxed(muensk_data.cpu_interface_raw_address + IRQ_ALIASED_ACKNOWLEDGE_OFFSET);
+	do
+	{
+		irq_status = readl_relaxed(muensk_data.raw_address + IRQ_ACKNOWLEDGE_OFFSET);
 		irq_number = irq_status & IRQ_ACKNOWLEDGE_MASK;
 
-		if (irq_number != IRQ_NO_PENDING_GROUP_1_VALUE && irq_number != IRQ_NO_PENDING_GROUP_0_VALUE) {
+		if (irq_number != IRQ_NO_PENDING_GROUP_1_VALUE &&
+			irq_number != IRQ_NO_PENDING_GROUP_0_VALUE)
+		{
+			writel_relaxed(irq_status, muensk_data.raw_address + IRQ_END_OF_INTERRUPT_OFFSET);
 			generic_handle_domain_irq(muensk_data.domain, irq_number);
 			continue;
 		}
@@ -182,32 +270,39 @@ static void __exception_irq_entry muensk_handle_irq(struct pt_regs *regs)
 	} while (1);
 }
 
-/*
- * ToDo
+
+/**
+ * muensk_component_address - Reads the start address of the
+ * irq controller address from the device tree.
+ *
+ * @node           :	the device tree node of the irq controller
+ * @resource_index :	the node index of the base address
+ *
+ * Returns the start address for success or an error otherwise.
  */
 unsigned long muensk_component_address(struct device_node *node, int resource_index)
 {
 	struct resource address_res;
 
 	if (of_address_to_resource(node, resource_index, &address_res) != 0) {
-		printk(KERN_INFO "ERROR %s: could not read physical address", muensk_data.chip.name);
+		printk(KERN_ERR "ERROR %s: could not read physical address", muensk_data.chip.name);
 		return -1;
 	}
 
 	return address_res.start;
 }
 
-bool muensk_write_read_assert(u32 register_value, volatile void __iomem *address)
-{
-	u32 register_temp;
-
-	writel_relaxed(register_value, address);
-	isb();
-	register_temp = readl_relaxed(address);
-
-	return register_temp & register_value;
-}
-
+/**
+ * muensk_init - Called by the Linux kernel init process. The
+ * Muen SK irq chip driver currently only supports a static
+ * configuration with group 0 enabled, group 1 disabled,
+ * default priority and binary point (c.f. description).
+ *
+ * @node   :	the device tree node of the irq controller
+ * @parent :	the parent device tree node of the irq controller
+ *
+ * Returns 0 for success or an error otherwise.
+ */
 static int __init muensk_init(struct device_node *node, struct device_node *parent)
 {
 	muensk_data.chip = muensk_chip;
@@ -219,22 +314,25 @@ static int __init muensk_init(struct device_node *node, struct device_node *pare
 		return -ENODEV;
 	}
 
-	muensk_data.distributor_physical_address = muensk_component_address(node, 0);
-	muensk_data.cpu_interface_physical_address = muensk_component_address(node, 1);
-
-	muensk_data.distributor_raw_address = of_iomap(node, 0);
-	muensk_data.cpu_interface_raw_address = of_iomap(node, 1);
+	muensk_data.physical_address = muensk_component_address(node, 0);
+	muensk_data.raw_address      = of_iomap(node, 0);
 
 	set_handle_irq(muensk_handle_irq);
 
-	muensk_data.domain = irq_domain_create_linear(&node->fwnode, NUMBER_OF_INTERRUPTS, &muensk_irq_domain_ops, &muensk_data);
+	muensk_data.domain = irq_domain_create_linear(
+		&node->fwnode, NUMBER_OF_INTERRUPTS, &muensk_irq_domain_ops, &muensk_data
+	);
+
+	writel_relaxed(IRQ_DEFAULT_CONTROL, muensk_data.raw_address + IRQ_CONTROL_OFFSET);
+	writel_relaxed(IRQ_DEFAULT_PRIORITY, muensk_data.raw_address + IRQ_PRIORITY_MASK_OFFSET);
+	writel_relaxed(IRQ_DEFAULT_BINARY_POINT, muensk_data.raw_address + IRQ_BINARY_POINT_OFFSET);
 
 	muensk_data.initialized = true;
 
-	printk(KERN_INFO "    DTS Node Name: %s", node->full_name);
-	printk(KERN_INFO "    Distributor Physical Address: %#lx", muensk_data.distributor_physical_address);
-	printk(KERN_INFO "    CPU Interface Physical Address: %#lx", muensk_data.cpu_interface_physical_address);
-	printk(KERN_INFO "    IRQ Status: %s", (muensk_data.initialized ? "initialization successful" : "initialization failed"));
+	printk(KERN_DEBUG "    DTS Node Name: %s", node->full_name);
+	printk(KERN_DEBUG "    Physical Address: %#lx", muensk_data.physical_address);
+	printk(KERN_DEBUG "    IRQ Status: %s", (muensk_data.initialized ?
+		"initialization successful" : "initialization failed"));
 
 	return muensk_data.initialized ? 0 : -1;
 }
