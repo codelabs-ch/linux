bb29a878766fe995120828dda7d2b04e1728972c
Reto Buerki <reet@codelabs.ch>
2023-03-31 16:59:51 +0200

    Revert "muen-smp: Let APs setup their sinfo"
    This reverts commit 9258015f0e4574a55b085b72201bb3f14254f597.
    
    muen_sinfo_setup uses ioremap_cache which cannot be called from atomic
    context.

--- drivers/muen/smp/muen-smp.c
+++ drivers/muen/smp/muen-smp.c
@@ -227,7 +227,6 @@ static void smp_callin(void)
 {
 	const int cpuid = smp_processor_id();
 
-	muen_sinfo_setup(cpuid);
 	muen_smp_store_cpu_info(cpuid);
 
 	set_cpu_sibling_map(raw_smp_processor_id());
@@ -359,6 +358,7 @@ int muen_cpu_up(unsigned int cpu, struct task_struct *tidle)
 	/* the FPU context is blank, nobody can own it */
 	per_cpu(fpu_fpregs_owner_ctx, cpu) = NULL;
 
+	muen_sinfo_setup(cpu);
 	common_cpu_up(cpu, tidle);
 
 	err = do_boot_cpu(cpu, tidle);
