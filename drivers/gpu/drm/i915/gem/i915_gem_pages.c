24144c0c7f65fc83096028cb51d04b37ab7a41c7
Adrian-Ken Rueegsegger <ken@codelabs.ch>
2024-01-09 16:39:00 +0100

    drm/i915: Do not return error on WC mappings
    On Muen, PAT is disabled for guests but the memory type for the
    requested memory resource is set via the system policy/hardware spec and
    enforced via EPT. Remove the warning messages and do not fail in this
    case.

--- drivers/gpu/drm/i915/gem/i915_gem_pages.c
+++ drivers/gpu/drm/i915/gem/i915_gem_pages.c
@@ -434,9 +434,13 @@ void *i915_gem_object_pin_map(struct drm_i915_gem_object *obj,
 			goto err_unpin;
 		}
 
+#ifdef CONFIG_MUEN_GUEST
+		if (i915_gem_object_has_struct_page(obj))
+#else
 		if (GEM_WARN_ON(type == I915_MAP_WC && !pat_enabled()))
 			ptr = ERR_PTR(-ENODEV);
 		else if (i915_gem_object_has_struct_page(obj))
+#endif
 			ptr = i915_gem_object_map_page(obj, type);
 		else
 			ptr = i915_gem_object_map_pfn(obj, type);
